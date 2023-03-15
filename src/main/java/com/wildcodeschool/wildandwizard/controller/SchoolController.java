0e372fb7048e03adc9e462a641e57fd806095517
Alexandre Doucet <alexandredoucet.fr@gmail.com>
2023-03-15 14:17:09 +0100

    first commit

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,8 @@
 package com.wildcodeschool.wildandwizard.controller;
-
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SchoolController {
 
     // TODO : get school repository by dependency injection
+    @Autowired
+    private SchoolRepository repository;
+
 
     @GetMapping("/schools")
     public String getAll(Model model) {
 
-        // TODO : find all schools
+       // find all schools
+        model.addAttribute("schools", repository.findAll());
 
         return "schools";
     }
@@ -25,7 +31,15 @@ public class SchoolController {
     public String getSchool(Model model,
                             @RequestParam(required = false) Long id) {
 
-        // TODO : find one school by id
+        
+        School school = new School();
+        if (id != null) {           // find school by id
+            Optional<School> optionalSchool = repository.findById(id);
+            if (optionalSchool.isPresent()) {
+                school = optionalSchool.get();
+            }
+        }
+        model.addAttribute("school", school);
 
         return "school";
     }
@@ -35,6 +49,8 @@ public class SchoolController {
 
         // TODO : create or update a school
 
+
+        repository.save(school);
         return "redirect:/schools";
     }
 
@@ -42,7 +58,7 @@ public class SchoolController {
     public String deleteSchool(@RequestParam Long id) {
 
         // TODO : delete a school
-
+        repository.deleteById(id);
         return "redirect:/schools";
     }
 }
